57bfbc9de9754c46012ce740ce1182f7de3a2309
Tatsuhiko Arai <tatsu@tatsu.com>
2016-11-26 20:43:54 +0900

    Finish THE HERO EDITOR

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,8 +3,18 @@
 
 import 'package:angular2/core.dart';
 
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
+
 @Component(
     selector: 'my-app',
     styleUrls: const ['app_component.css'],
     templateUrl: 'app_component.html')
-class AppComponent {}
+class AppComponent {
+  String title = 'Tour of Heroes';
+  Hero hero = new Hero(1, 'Windstorm');
+}
