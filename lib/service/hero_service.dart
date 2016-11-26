9782a6ec38fbb652f8467263106c498d6dbabf28
Tatsuhiko Arai <tatsu@tatsu.com>
2016-11-27 07:48:32 +0900

    Simulate a slow connection

--- lib/service/hero_service.dart
+++ lib/service/hero_service.dart
@@ -6,5 +6,10 @@ import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
-  Future<List<Hero>> getHeroes() async => mockHeroes;
+  Future<List<Hero>> getHeroes() async {
+    // Simulate a slow connection
+    return new Future.delayed(const Duration(seconds: 2),
+        () => mockHeroes
+    );
+  }
 }
