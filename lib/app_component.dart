6d19abe6fc7af3695cea66bf73bb5e2fc3b9d75b
Tatsuhiko Arai <tatsu@tatsu.com>
2016-11-26 21:09:55 +0900

    Finish MASTER/DETAIL

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -15,6 +15,24 @@ class Hero {
     styleUrls: const ['app_component.css'],
     templateUrl: 'app_component.html')
 class AppComponent {
-  String title = 'Tour of Heroes';
-  Hero hero = new Hero(1, 'Windstorm');
+  final String title = 'Tour of Heroes';
+  final List<Hero> heroes = mockHeroes;
+  Hero selectedHero;
+
+  onSelect(Hero hero) {
+    selectedHero = hero;
+  }
 }
+
+final List<Hero> mockHeroes = [
+  new Hero(11, 'Mr. Nice'),
+  new Hero(12, 'Narco'),
+  new Hero(13, 'Bombasto'),
+  new Hero(14, 'Celeritas'),
+  new Hero(15, 'Magneta'),
+  new Hero(16, 'RubberMan'),
+  new Hero(17, 'Dynama'),
+  new Hero(18, 'Dr IQ'),
+  new Hero(19, 'Magma'),
+  new Hero(20, 'Tornado')
+];
