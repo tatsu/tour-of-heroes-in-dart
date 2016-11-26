8e36379020836e935dbfd921117c0db5e204e3d9
Tatsuhiko Arai <tatsu@tatsu.com>
2016-11-27 07:40:23 +0900

    Finish SERVICES

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,35 +1,35 @@
-// Copyright (c) 2016, tatsu. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
+import 'dart:async';
 import 'package:angular2/core.dart';
 
 import 'hero.dart';
 import 'hero_detail/hero_detail_component.dart';
+import 'service/hero_service.dart';
 
 @Component(
     selector: 'my-app',
     styleUrls: const ['app_component.css'],
     templateUrl: 'app_component.html',
-    directives: const [HeroDetailComponent])
-class AppComponent {
+    directives: const [HeroDetailComponent],
+    providers: const [HeroService])
+class AppComponent implements OnInit {
   final String title = 'Tour of Heroes';
-  final List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
   Hero selectedHero;
+
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
+
+  @override
+  ngOnInit() {
+    getHeroes();
+  }
+
+  Future<Null> getHeroes() async {
+    heroes = await _heroService.getHeroes();
+  }
 
   onSelect(Hero hero) {
     selectedHero = hero;
   }
 }
-
-final List<Hero> mockHeroes = [
-  new Hero(11, 'Mr. Nice'),
-  new Hero(12, 'Narco'),
-  new Hero(13, 'Bombasto'),
-  new Hero(14, 'Celeritas'),
-  new Hero(15, 'Magneta'),
-  new Hero(16, 'RubberMan'),
-  new Hero(17, 'Dynama'),
-  new Hero(18, 'Dr IQ'),
-  new Hero(19, 'Magma'),
-  new Hero(20, 'Tornado')
-];
---
+++ lib/service/hero_service.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:angular2/core.dart';
+
+import '../hero.dart';
+import 'mock_heroes.dart';
+
+@Injectable()
+class HeroService {
+  Future<List<Hero>> getHeroes() async => mockHeroes;
+}
