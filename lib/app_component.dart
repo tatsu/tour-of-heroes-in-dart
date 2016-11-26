6a81f4613555d2c4a05bec11ec239859819751c3
Tatsuhiko Arai <tatsu@tatsu.com>
2016-11-26 20:13:49 +0900

    Initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, tatsu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'my-app',
+    styleUrls: const ['app_component.css'],
+    templateUrl: 'app_component.html')
+class AppComponent {}
---
+++ web/main.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, tatsu. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/platform/browser.dart';
+
+import 'package:dart_tour_of_heroes/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
